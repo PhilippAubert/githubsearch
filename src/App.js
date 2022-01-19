2cec6a33a092ef68ca395e5a7165b80462fbacd3
philipp aubert <p.b.m.aubert@gmail.com>
2022-01-19 19:35:12 +0100

    Initial commit. Setup basic fetch with example-user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("error", error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GIT HUB USERS</h1>
+      <p>{data.url}</p>
     </div>
   );
 }
