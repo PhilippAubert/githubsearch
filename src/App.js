5d09a81ac79ef23437f09c75e764a86de4851d3d
philipp aubert <p.b.m.aubert@gmail.com>
2022-01-19 22:26:01 +0100

    Add authentication token. Testing out api-fetch working

--- src/App.js
+++ src/App.js
@@ -3,18 +3,41 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [userName, setUserName] = useState("example");
 
   useEffect(() => {
-    fetch("https://api.github.com/users/example")
+    fetch(`https://api.github.com/users/${userName}`, {
+      method: "GET",
+      headers: {
+        Authorization: `${process.env.TOKEN}`,
+      },
+    })
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("error", error));
-  }, []);
+  }, [userName]);
+
+  function handleSearch(e) {
+    e.preventDefault();
+    const newUser = e.target.value;
+    if (newUser.length <= 2) {
+      setUserName("");
+    } else {
+      setUserName(newUser);
+    }
+  }
 
   return (
     <div className="App">
       <h1>GIT HUB USERS</h1>
-      <p>{data.url}</p>
+      <form>
+        <input type="text" onInput={handleSearch} />
+      </form>
+      <div>
+        <p>{data.url}</p>
+        <img src={data.avatar_url} alt="user_pic" />
+        <p>{data.bio}</p>
+      </div>
     </div>
   );
 }
